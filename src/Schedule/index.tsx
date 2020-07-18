5e6ba43918fd7e4502995289cedc8c2256a3f0b8
Vito Breda <vito_odoni@hotmail.com>
2020-07-18 15:22:17 -0300

    add object to schedule

--- src/Schedule/index.tsx
+++ src/Schedule/index.tsx
@@ -1,43 +1,34 @@
-import React, {useState} from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
-import {Agenda} from 'react-native-calendars';
-import {Card, Avatar} from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { Agenda } from 'react-native-calendars';
+import { Card, Avatar } from 'react-native-paper';
 
-const timeToString = (time) => {
+interface ObjectItem {
+  height: number,
+  name: string,
+}
+
+
+function timeToString(time: string) {
   const date = new Date(time);
+
   return date.toISOString().split('T')[0];
 };
 
 export default function Schedule() {
   const [items, setItems] = useState({});
 
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
+  function loadItems(day: any) {
+
+    setItems({
+      '2020-07-18': [{ name: 'item 1 - any js object abc' }],
+    })
+
   };
 
-  const renderItem = (item) => {
+  const renderItem = (item: ObjectItem) => {
     return (
-      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+      <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
         <Card>
           <Card.Content>
             <View
@@ -56,12 +47,18 @@ export default function Schedule() {
   };
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <Agenda
         items={items}
-        loadItemsForMonth={loadItems}
-        selected={'2017-05-16'}
+        loadItemsForMonth={(month) => { loadItems(month) }}
+        selected={'2020-07-18'}
         renderItem={renderItem}
+        renderEmptyDate={() => { return (<View />); }}
+        rowHasChanged={(r1, r2) => { return r1.name !== r2.name }}
+        markingType={'multi-dot'}
+        markedDates={{
+          '2020-07-18': {dots: [{key:'massage', color: 'red', selectedDotColor: 'red'}]},
+        }}
       />
     </View>
   );
